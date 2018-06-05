579bbe603366b0f0452ea5da6d6769776de09733
ankitmundada <aomundada@gmail.com>
2018-06-05 12:13:48 +0530

    The script now returns csv and png outputs

--- json_to_csv_plot.py
+++ json_to_csv_plot.py
@@ -2,11 +2,13 @@ import json
 import csv
 from mpl_toolkits.mplot3d import Axes3D
 import argparse
+import matplotlib
+matplotlib.use('agg')
 import matplotlib.pyplot as plt
-from matplotlib import cm
 import numpy as np
 import pandas as pd
-from sys import argvimport argparse
+from sys import argv
+import argparse
 
 parser = argparse.ArgumentParser(description='JSON to CSV')
 parser.add_argument('--json_path',  default=None)
@@ -26,10 +28,12 @@ with open(args.json_path, 'r') as f:
         for res in sorted_j:
             c.writerow([res[0], res[1], res[2], res[3], res[4], res[5], res[6]])
 
+    print("Best params: {}".format(sorted_j[0]))
+
 df = pd.read_csv(args.json_path.replace('.json', '.csv'), engine="python")
 fig = plt.figure()
 ax = Axes3D(fig)
-surf = ax.plot_trisurf(df.lm_alpha.values, df.lm_beta.values, df.wer.values, cmap=cm.jet, linewidth=0.1)
+surf = ax.plot_trisurf(df.lm_alpha.values, df.lm_beta.values, df.wer.values, cmap=matplotlib.cm.jet, linewidth=0.1)
 fig.colorbar(surf, shrink=0.5, aspect=5)
-plt.savefig(args.json_path.replace('.csv','.svg'))
+plt.savefig(args.json_path.replace('.json','.png'))
 
